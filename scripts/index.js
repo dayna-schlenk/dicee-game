cd130e241bef49e166bc91b55176ad77f24c965b
dayna-schlenk <dayna.schlenker@gmail.com>
2021-02-13 11:13:15 -0600

    Add js to change both dice images at random upon page refresh

--- scripts/index.js
+++ scripts/index.js
@@ -1,2 +1,8 @@
-// Variables
-let randomNumber1 = Math.floor(Math.random() * 6) + 1;
+var randomNumber1 = Math.floor(Math.random() * 6) + 1;
+var randomNumber2 = Math.floor(Math.random() * 6) + 1;
+
+var image1 = "/images/dice" + randomNumber1 + ".png";
+var image2 = "/images/dice" + randomNumber2 + ".png";
+
+document.querySelector(".dice .img1").setAttribute("src", image1);
+document.querySelector(".dice .img2").setAttribute("src", image2);

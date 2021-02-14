2fe4e7ae0569bdfb1c07ca9606ebd188dfd140cc
dayna-schlenk <dayna.schlenker@gmail.com>
2021-02-14 08:17:26 -0600

    Change text in h1 depending on results of dice roll

--- scripts/index.js
+++ scripts/index.js
@@ -9,3 +9,10 @@ document.querySelector(".dice .img1").setAttribute("src", image1);
 document.querySelector(".dice .img2").setAttribute("src", image2);
 
 // Change h1 depending on results of dice roll
+if (randomNumber1 > randomNumber2) {
+    document.querySelector("h1").innerHTML = "🚩 Player 1 Wins!";
+} else if (randomNumber1 < randomNumber2) {
+    document.querySelector("h1").innerHTML = "Player 2 Wins! 🚩";
+} else {
+    document.querySelector("h1").innerHTML = "Draw!";
+}

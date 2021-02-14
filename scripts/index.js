b058b97d54a7f529f48b17ffce83e74e92932060
dayna-schlenk <dayna.schlenker@gmail.com>
2021-02-14 08:02:12 -0600

    Add comments to js file

--- scripts/index.js
+++ scripts/index.js
@@ -1,3 +1,4 @@
+// Generate random numbers and random dice images upon refresh
 var randomNumber1 = Math.floor(Math.random() * 6) + 1;
 var randomNumber2 = Math.floor(Math.random() * 6) + 1;
 
@@ -6,3 +7,5 @@ var image2 = "/images/dice" + randomNumber2 + ".png";
 
 document.querySelector(".dice .img1").setAttribute("src", image1);
 document.querySelector(".dice .img2").setAttribute("src", image2);
+
+// Change h1 depending on results of dice roll

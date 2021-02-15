078114ba12970973e77debb0e2f107b2dad85c68
dayna-schlenk <dayna.schlenker@gmail.com>
2021-02-15 06:30:03 -0600

    Change icons for results of dice roll

--- scripts/index.js
+++ scripts/index.js
@@ -10,9 +10,9 @@ document.querySelector(".dice .img2").setAttribute("src", image2);
 
 // Change h1 depending on results of dice roll
 if (randomNumber1 > randomNumber2) {
-    document.querySelector("h1").innerHTML = "🚩 Player 1 Wins!";
+    document.querySelector("h1").innerHTML = "🎉 Player 1 Wins!";
 } else if (randomNumber1 < randomNumber2) {
-    document.querySelector("h1").innerHTML = "Player 2 Wins! 🚩";
+    document.querySelector("h1").innerHTML = "Player 2 Wins! 🎉";
 } else {
-    document.querySelector("h1").innerHTML = "Draw!";
+    document.querySelector("h1").innerHTML = "Draw 😐";
 }
